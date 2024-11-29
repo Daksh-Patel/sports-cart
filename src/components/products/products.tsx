ef0d3e4b33730b96c4cf6e2f4e3dfc48c296bda5
Daksh Patel <dakshpatel@Dakshs-MacBook-Air.local>
2024-11-29 14:54:07 +0530

    changes for the products section

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -10,7 +10,7 @@ const Products = () => {
       <div className='container'>
         <h2 className='text-4xl font-bold'>Our Products</h2>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3 md:gap-4 xl:gap-6 my-10'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-4 xl:gap-6 my-10'>
           {ProductItems.map((product) => (
             <ProductCard
               key={product.id}
